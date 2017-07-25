21b906a6001ae54c32e4adbeec0e10e0801903c2
Milnes, Rob - Rob <vc.rob.milnes@lowes.com>
2017-07-25 15:02:27 -0400

    initial

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        less: {
+            development: {
+                options: {
+                    compress: true,
+                    cleancss: true,
+                    optimization: 2,
+                    dumpLineNumbers: 'false'
+                },
+                files: {
+                    "css/genesis.css": "less/genesis.less"
+                }
+            }
+        },
+
+        watch: {
+            options: {
+                livereload: false,
+            },
+            styles: {
+                files: ['less/**/*.less'],
+                tasks: ['less'],
+                options: {
+                    nospawn: true
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['less', 'watch']);
+};
---
+++ README.md
@@ -0,0 +1,34 @@
+# Skeleton Playground for Genesis
+
+This is essentially a playground for Genesis interactivity and the likes.
+
+-----
+
+## Getting started
+
+### Install Global Dependencies
+  * [Node.js](http://nodejs.org)
+  * [Bower](http://bower.io): `[sudo] npm install bower -g`
+  * [Grunt.js](http://gruntjs.com): `[sudo] npm install -g grunt-cli`
+
+### Install Local Dependencies
+  * cd to project folder
+  * run `[sudo] npm install` (first time)
+  * run `grunt` (to watch and compile Less files)
+
+### What should it look like?
+
+Whenever you run the above, you should essentially have the following;
+
+```
+Genesis/
+├── index.html
+├── less/
+│   └── genesis.less
+├── images/
+│   └── favicon.png
+├── package.json
+├── Gruntfile.js
+└── README.md
+
+```
